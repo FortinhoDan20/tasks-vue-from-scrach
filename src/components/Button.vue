c1fc16f52049142ae175d94b4d6263880d8f6c84
Fortinho Vihamba <fortinhovihamba@iMac-de-CMCT-2.local>
2021-09-22 13:51:46 +0100

    ajout de components

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="getClick()" :style="{ background: color }" class="btn" > {{ text}} </button>
+</template>
+
+<script>
+export default {
+    name: "Buttom",
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        getClick() {
+            console.log('click')
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
